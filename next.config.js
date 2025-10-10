fdf768b44c2a2b6858bf9f9bd17cbeb3ed534b36
kkeelor <kkeelor@gmail.com>
2025-10-10 23:29:06 +0530

    Fix backend URL

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://tradeclarity-backend-r612dddom-kkeelors-projects.vercel.app'
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL 
   }
 }
 
